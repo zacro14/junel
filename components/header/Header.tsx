44f6bbe84127023971277555f2b0a2a94b347b23
junel <sacrojunel@gmail.com>
2022-11-14 18:51:41 +0800

    feat(navigation): add navigation(#1)

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,19 +1,38 @@
-import { header } from "data/header";
+import { header } from 'data/header'
+import Link from 'next/link'
+import { Bars3BottomLeftIcon } from '@heroicons/react/24/outline'
+import { useRouter } from 'next/router'
 
 export const Header = () => {
-  return (
-    <section className="h-screen grid items-center justify-center bg-main">
-      <nav className="grid grid-cols-2 grid-rows-2 items-center justify-centers gap-5 cursor-pointer ">
-        {header.map(({ name }) => (
-          <div
-            key={name}
-            className="h-52 w-52 bg-secondary flex items-center justify-center rounded-2xl select-none"
-          >
-            <h1 className="text-stroke text-xl uppercase">{name}</h1>
-          </div>
-        ))}
-        \
-      </nav>
-    </section>
-  );
-};
+    const router = useRouter()
+
+    console.log(router)
+    return (
+        <header className="shadow-2xl my-10 mx-10 p-5 rounded-xl">
+            <div className="grid justify-end items-center md:hidden lg:hidden">
+                <Bars3BottomLeftIcon className="h-10 w-10" />
+            </div>
+            <ul className="hidden grid-cols-5 justify-end md:grid lg:grid gap-3">
+                {header.map(({ name, link }) => (
+                    <Link href={link} passHref key={name}>
+                        <li
+                            key={name}
+                            className={`text-stroke text-center hover:bg-secondary p-10 cursor-pointer rounded-md ${
+                                router.pathname === link && 'bg-secondary  shadow-md'
+                            }`}
+                        >
+                            <p
+                                className={`h-full capitalize first-letter:text-2xl  ${
+                                    router.pathname === link &&
+                                    'font-bold text-stroke'
+                                }`}
+                            >
+                                {name}
+                            </p>
+                        </li>
+                    </Link>
+                ))}
+            </ul>
+        </header>
+    )
+}
