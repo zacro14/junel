224108acba85da034d65e7ef9d7d1008115728fd
junel <sacrojunel@gmail.com>
2022-11-15 17:28:04 +0800

    feat: add my contact page (#4)

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ContactIcon from 'images/social.svg'
 import Image from 'next/image'
 import { contact } from 'data/contact'
+import Seo from 'components/seo'
 
 const Contact = () => {
     return (
         <div className="container mx-auto">
-            <div className="grid grid-cols-2 items-center justify-center h-64">
+            <Seo title="Junel Sacro | Contact" description="Contact me" />
+            <div className="grid grid-rows-2 md:grid-cols-2 lg:grid-cols-2 items-center justify-center">
                 <span className="full p-2 border-secondary border">
                     <Image
                         className="h-full"
