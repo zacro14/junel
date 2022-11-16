34ea1ee274278192023b19c95df5c19d342fcac1
junel <sacrojunel@gmail.com>
2022-11-16 19:08:26 +0800

    :rocket feat: add tech stack page

--- pages/tech-stack.tsx
+++ pages/tech-stack.tsx
@@ -3,16 +3,56 @@ import React from 'react'
 
 const TechStack = () => {
     return (
-        <div>
-            <Seo title="Tech Stack" />
-            <h1 className="font-bold">Technologies</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-5">
-                <div className="bg-main h-52 w-full">NextJs</div>
-                <div className="bg-secondary h-36  w-full ">Chakra UI</div>
-                <div className="bg-main h-36  w-full ">Tailwind</div>
-                <div className="bg-main h-36  w-full ">Typescript</div>
+        <section>
+            <Seo title="TJunel Sacro | Tech-Stack" />
+            <div className="grid gap-10">
+                <h1 className="font-bold">Technologies</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                    <div className="bg-main h-52 w-full rounded-md flex items-center justify-center">
+                        <h2 className="font-bold">Nextjs</h2>
+                    </div>
+
+                    <div className="bg-secondary h-52 w-full rounded-md flex items-center justify-center">
+                        <h2 className="font-bold">Chakra Ui</h2>
+                    </div>
+
+                    <div className="bg-main h-52 w-full rounded-md flex items-center justify-center">
+                        <h2 className="font-bold">Tailwind css</h2>
+                    </div>
+                    <div className="bg-secondary h-52 w-full rounded-md flex items-center justify-center">
+                        <h2 className="font-bold">Typescript</h2>
+                    </div>
+                    <div className="bg-main h-52 w-full rounded-md flex items-center justify-center">
+                        <h2 className="font-bold">Javascript</h2>
+                    </div>
+                    <div className="bg-secondary h-52 w-full rounded-md flex items-center justify-center">
+                        <h2 className="font-bold">Expressjs</h2>
+                    </div>
+                    <div className="bg-main h-52 w-full rounded-md flex items-center justify-center">
+                        <h2 className="font-bold">Moongose</h2>
+                    </div>
+                    <div className="bg-secondary h-52 w-full rounded-md flex items-center justify-center">
+                        <h2 className="font-bold">Mongodb</h2>
+                    </div>
+                </div>
+                <div>
+                    <h1 className="font-bold">Development tools</h1>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-5 ">
+                        <div className="bg-main h-52 w-full rounded-md flex items-center justify-center">
+                            <h2 className="font-bold">Git</h2>
+                        </div>
+
+                        <div className="bg-main h-52 w-full rounded-md flex items-center justify-center">
+                            <h2 className="font-bold">Vscode</h2>
+                        </div>
+
+                        <div className="bg-main h-52 w-full rounded-md flex items-center justify-center">
+                            <h2 className="font-bold">Postman</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
