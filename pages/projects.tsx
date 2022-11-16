fde092b3ed4256ee3a2a2c68ccc3b0ee0fecc06e
junel <sacrojunel@gmail.com>
2022-11-16 19:12:51 +0800

    feat: (): add projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,17 @@
+import Seo from 'components/seo'
 import React from 'react'
 
 const Projects = () => {
-    return <div>Projects</div>
+    return (
+        <div className="flex items-center">
+            <Seo title="Junel Sacro | Projects" />
+            <div className="grid w-full mx-10 md:mx-52 lg:52">
+                <div className="h-96 w-full bg-secondary rounded-3xl">
+                    MusicPhile
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Projects
