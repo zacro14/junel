5b721adaec8c7c8a77e52412c67c05cba151b83d
junel <sacrojunel@gmail.com>
2022-11-17 18:31:08 +0800

    feat : add featured projects

--- components/Projects/ProjectItem.tsx
+++ components/Projects/ProjectItem.tsx
@@ -1,9 +1,42 @@
+import Image from 'next/image'
 import React from 'react'
 interface IProjects {
     name: string
     description: string
+    image: any
+    tags: string[]
 }
 
-export const ProjectItem: React.FC<IProjects> = ({ name, description }) => {
-    return <div></div>
+export const ProjectItem: React.FC<IProjects> = ({
+    name,
+    description,
+    image,
+    tags,
+}) => {
+    return (
+        <div className=" min-h-[400px] w-full bg-secondary rounded-3xl p-5 flex flex-col gap-5">
+            <div className="select-none">
+                <Image
+                    src={image}
+                    alt={name}
+                    placeholder={'blur'}
+                    blurDataURL={image.src}
+                    className={'rounded-xl object-cover bg-main shadow-xl'}
+                />
+            </div>
+            <div>
+                <div className="capitalize text-lg font-bold mb-2">{name}</div>
+                <div className="text-opacity-75 text-stroke text-sm">
+                    {description}
+                </div>
+            </div>
+            <div className="flex flex-wrap gap-2">
+                {tags.map((tags) => (
+                    <span className="rounded-full bg-stroke text-main py-2 px-5 bg-opacity-75 mr-2">
+                        {tags}
+                    </span>
+                ))}
+            </div>
+        </div>
+    )
 }
--- data/projects.ts
+++ data/projects.ts
@@ -1,6 +1,11 @@
+import musicphile from 'images/musicphile.png'
+
 export const projects = [
     {
-        name: 'MusicPhile',
-        description: 'Music App ',
+        name: 'Musicphile',
+        description:
+            'A front end website built for music, it used static songs from shazam api core and can play music.',
+        image: musicphile,
+        tags: ['Nextjs', 'Typescript', 'Redux toolkit', 'Tailwind css'],
     },
 ]
